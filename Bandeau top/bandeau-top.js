2a774f935f4992639c3ba492776cb990ee5160a8
ivainbordeneuve <ivain.bordeneuve@gmail.com>
2021-09-21 12:08:32 +0200

    Ajout bandeau Top

---
+++ Bandeau top/bandeau-top.js
@@ -0,0 +1,3 @@
+
+document.querySelector('#header')
+.insertAdjacentHTML('afterbegin', '<div class="alert-message"><a href="https://www.n-py.com/fr/faq-covid-2021">Garanties Covid 2021-2022  <img src="https://redmine.n-py.com/Test/Adelya/Temp/img/fleche-lien-white.png" alt=""></a></div>');
---
+++ bandeau-top.js
@@ -0,0 +1,2 @@
+
+document.querySelector('#header').insertAdjacentHTML('afterbegin', '<div class="alert-message"><a href="https://www.n-py.com/fr/grand-tourmalet/info-pistes-securite">Domaine skiable fermé: Consulter toutes les infos relatives aux conditions d\'accès sur le domaine non sécurisé <img src="https://redmine.n-py.com/Test/Adelya/Temp/img/fleche-lien-white.png" alt=""></a></div>');
